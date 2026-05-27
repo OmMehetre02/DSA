b7de07361ab4ac8ac66983aea67cd57d94ade1b6
OmMehetre02 <ommehetrelonar@gmail.com>
2026-05-27 19:37:17 +0530

    add code

---
+++ OPPS/Class_Object/OOPS1.java
@@ -0,0 +1,14 @@
+class pen{
+	String color;
+
+public void write(){
+	
+		System.out.println("Somthing write");
+}
+}
+public class OOPS1{
+	public static void main(String args[]){
+		 pen pen1=new pen();
+		 pen1.color="blue";
+	}
+}
